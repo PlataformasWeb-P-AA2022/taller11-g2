28bb45b67ac1ebf04a030da4be22ae5ef51c4f6a
msmartinez12 <msmartinez12@utpl.edu.ec>
2022-06-28 15:44:08 -0500

    cambios

--- taller/taller11/administrativo/urls.py
+++ taller/taller11/administrativo/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
-
+        path('crear/edificio', views.crear_edificio, name='crear_edifico'),
+        path('crear/departamento', views.crear_departamento, name='crear_departamento'),
+        path('edicio/<int:id>', views.obtener_edificio, name='obtener_edificio')
  ]
--- taller/taller11/administrativo/views.py
+++ taller/taller11/administrativo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from administrativo.models import *
 
 # Create your views here.
@@ -6,3 +6,46 @@ def index(request):
     edificios = Edificio.objects.all()
     informacion_template = {'edificios': edificios, 'numero_edificios': len(edificios)}
     return render(request, 'index.html', informacion_template)
+
+def crear_edificio(request):
+    if request.method=='POST':
+        formulario = EdificioForm(request.POST)
+        print(formulario.errors)
+        if formulario.is_valid():
+            formulario.save() # se guarda en la base de datos
+            return redirect(index)
+    else:
+        formulario = EdificioForm()
+    diccionario = {'formulario': formulario}
+
+    return render(request, 'crearEdificio.html', diccionario)
+
+
+def crear_departamento(request):
+    if request.method=='POST':
+        formulario = DepartamentoForm(request.POST)
+        print(formulario.errors)
+        if formulario.is_valid():
+            formulario.save() # se guarda en la base de datos
+            return redirect(index)
+    else:
+        formulario = DepartamentoForm()
+    diccionario = {'formulario': formulario}
+
+    return render(request, 'crearEdificio.html', diccionario)
+
+def obtener_edificio(request, id):
+    """
+        Listar los registros del modelo Estudiante,
+        obtenidos de la base de datos.
+    """
+    # a través del ORM de django se obtiene
+    # los registros de la entidad; el listado obtenido
+    # se lo almacena en una variable llamada
+    # estudiantes
+    edificio = Edificio.objects.get(pk=id)
+    # en la variable tipo diccionario llamada informacion_template
+    # se agregará la información que estará disponible
+    # en el template
+    informacion_template = {'edificio': edificio}
+    return render(request, 'obtenerEdificio.html', informacion_template)
