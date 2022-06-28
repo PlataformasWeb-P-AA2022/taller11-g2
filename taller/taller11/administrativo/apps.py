c57e1ff5d8b30b77412b2d2a62ecb6f01496a6cc
msmartinez12 <msmartinez12@utpl.edu.ec>
2022-06-28 14:47:58 -0500

    platilla index

--- taller/taller11/administrativo/apps.py
+++ taller/taller11/administrativo/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AdministrativcConfig(AppConfig):
+class AdministrativoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'administrativc'
+    name = 'administrativo'
